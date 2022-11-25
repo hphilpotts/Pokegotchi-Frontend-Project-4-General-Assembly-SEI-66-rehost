8d55bfef4464322d7b29bd00502175d5d46fe0c1
Henry Philpotts <hphilpotts@gmail.com>
2022-11-25 20:07:19 +0000

    Updated Card.js with default Axios POST header due to CORS issue.

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 import Axios from 'axios'
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 // MUI imports:
 import { Box } from '@mui/system'
