a0feb5d21958eee815795f08e53436dfbbcc99a5
Henry Philpotts <hphilpotts@gmail.com>
2022-11-25 19:25:36 +0000

    Updated all backend Axios calls with full URLs.

--- src/card/Card.js
+++ src/card/Card.js
@@ -79,7 +79,7 @@ export default function Card(props) {
   
   const feedFunction = pokegotchi => {
     const value = pG.foodLevel + 1
-    Axios.put(`pokegotchi/update?id=${props.user.user.id}&field=foodLevel&value=${value}`, pokegotchi)
+    Axios.put(`https://fair-red-bighorn-sheep-tutu.cyclic.app/pokegotchi/update?id=${props.user.user.id}&field=foodLevel&value=${value}`, pokegotchi)
     .then(res => {
       console.log('PokeGotchi updated!')
       console.log(res)
@@ -92,7 +92,7 @@ export default function Card(props) {
   }
   const cleanFunction = pokegotchi => {
     const value = pG.cleanLevel + 1
-    Axios.put(`pokegotchi/update?id=${props.user.user.id}&field=cleanLevel&value=${value}`, pokegotchi)
+    Axios.put(`https://fair-red-bighorn-sheep-tutu.cyclic.app/pokegotchi/update?id=${props.user.user.id}&field=cleanLevel&value=${value}`, pokegotchi)
     .then(res => {
       console.log('PokeGotchi updated!')
       console.log(res)
@@ -105,7 +105,7 @@ export default function Card(props) {
   }
   const playFunction = pokegotchi => {
     const value = pG.playLevel + 1
-    Axios.put(`pokegotchi/update?id=${props.user.user.id}&field=playLevel&value=${value}`, pokegotchi)
+    Axios.put(`https://fair-red-bighorn-sheep-tutu.cyclic.app/pokegotchi/update?id=${props.user.user.id}&field=playLevel&value=${value}`, pokegotchi)
     .then(res => {
       console.log('PokeGotchi updated!')
       console.log(res)
@@ -119,7 +119,7 @@ export default function Card(props) {
 
   const healFunction = pokegotchi => {
     // console.log(props.user.user.id)
-    Axios.put(`pokegotchi/update?id=${props.user.user.id}&field=hp&value=100`, pokegotchi)
+    Axios.put(`https://fair-red-bighorn-sheep-tutu.cyclic.app/pokegotchi/update?id=${props.user.user.id}&field=hp&value=100`, pokegotchi)
     .then(res => {
       console.log('PokeGotchi updated!')
       console.log(res)
